69048a9698ce3cc9dab83899e8a062691c9cbaa7
Sagar Maheshwary <sagarmaheshwary31@gmail.com>
2021-08-15 13:45:33 +0500

    added missing db config in src/config

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,4 +1,12 @@
 export default () => ({
   env: process.env.APP_ENV,
   port: parseInt(process.env.APP_PORT),
+  database: {
+    connection: process.env.DB_CONNECTION,
+    host: process.env.DB_HOST,
+    port: parseInt(process.env.DB_PORT),
+    dbname: process.env.DB_DATABASE,
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+  },
 });
